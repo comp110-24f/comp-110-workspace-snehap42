e1aeb7d2527292f31b7234d0ca3d145714109999
snehap42 <snehap@unc.edu>
2024-08-28 09:02:38 -0400

    Finished CQ1 Functions!

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,17 @@
+"""CQ Question 1: Mimic Function"""
+
+__author__ = "730740674"
+
+
+def mimic(message: str) -> str:
+    """Return message function"""
+    return message
+
+
+def main() -> None:
+    """main function to print mimic"""
+    print(mimic(message=input("What is your message?")))
+
+
+if __name__ == "__main__":
+    main()
