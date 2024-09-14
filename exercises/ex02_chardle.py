a363b2e73501ebfaccce3d4660867fe8c40e791d
snehap42 <snehap@unc.edu>
2024-09-14 10:36:28 -0400

    Finished Ex02!

---
+++ exercises/ex02_chardle.py
@@ -0,0 +1,57 @@
+"""EX02 - Chardle - A cute step toward Wordle."""
+
+__author__ = "730740674"
+
+
+def input_word() -> str:
+    """function to take input word that is 5 letters and exits if not"""
+    word: str = input("Enter a 5-character word: ")
+    if len(word) != 5:
+        print("Error: Word must contain 5 characters.")
+        exit()
+    return word
+
+
+def input_letter() -> str:
+    """function to take input letter that is 1 letter and exits if not"""
+    letter: str = input("Enter a single character: ")
+    if len(letter) != 1:
+        print("Error: Character must be a single character.")
+        exit()
+    return letter
+
+
+def contains_char(word: str, letter: str) -> None:
+    """function to check if letter matches at each index in word and keeps count"""
+    count = 0
+    print("Searching for " + letter + " in " + word)
+    if letter == word[0]:
+        print(letter + " found at index 0")
+        count = count + 1
+    if letter == word[1]:
+        print(letter + " found at index 1")
+        count = count + 1
+    if letter == word[2]:
+        print(letter + " found at index 2")
+        count = count + 1
+    if letter == word[3]:
+        print(letter + " found at index 3")
+        count = count + 1
+    if letter == word[4]:
+        print(letter + " found at index 4")
+        count = count + 1
+    if count == 0:
+        print("No instances of " + letter + " found in " + word)
+    elif count == 1:
+        print("1 instance of " + letter + " found in " + word)
+    else:
+        print(str(count) + " instances of " + letter + " found in " + word)
+
+
+def main() -> None:
+    """main function to centralize other functions"""
+    contains_char(word=input_word(), letter=input_letter())
+
+
+if __name__ == "__main__":
+    main()
