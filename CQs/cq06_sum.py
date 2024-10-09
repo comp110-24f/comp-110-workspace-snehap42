fcac5488be62492c7eeedb9bfacddbe0a662ef66
snehap42 <snehap@unc.edu>
2024-10-09 08:23:18 -0400

    Finished CQ06!

---
+++ CQs/cq06_sum.py
@@ -0,0 +1,29 @@
+"""Summing the elements of a list using different loops"""
+
+__author__ = "730740674"
+
+
+def w_sum(vals: list[float]) -> float:
+    """function to sum with a while loop"""
+    sum: float = 0.0
+    idx: int = 0
+    while idx < len(vals):
+        sum += vals[idx]
+        idx += 1
+    return sum  # returns sum as float from while loop
+
+
+def f_sum(vals: list[float]) -> float:
+    """function to sum with a for loop"""
+    sum: float = 0.0
+    for val in vals:
+        sum += val
+    return sum  # returns sum as float from for loop
+
+
+def f_range_sum(vals: list[float]) -> float:
+    """function to sum with a for loop and range"""
+    sum: float = 0.0
+    for val in range(len(vals)):
+        sum += val
+    return sum  # returns sum as float from for loop and range
